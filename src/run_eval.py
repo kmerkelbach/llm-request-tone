ff3b332a3186d707cbd1865eb70c15ec3965f06a
Kilian Merkelbach <kilian.merkelbach@gmail.com>
2025-08-19 10:19:09 +0200

    Finished templating for all GSM8K tasks

--- src/run_eval.py
+++ src/run_eval.py
@@ -4,7 +4,8 @@ from evaluation.lm_eval_shell import run_eval
 if __name__ == "__main__":
     d = run_eval(
         model="deepseek/deepseek-r1-distill-llama-8b",
-        tasks=["gsm8k_templated"],
+        tasks=["gsm8k_templated", "gsm8k_cot_templated", "gsm8k_cot_llama_templated",
+               "gsm8k_cot_self_consistency_templated", "gsm8k_cot_zeroshot_templated"],
         limit=1,
         num_concurrent=4,
         silent=False
--- tasks/lm_eval_task_overrides/templates/gsm8k/utils.py
+++ tasks/lm_eval_task_overrides/templates/gsm8k/utils.py
@@ -14,4 +14,16 @@ def _doc_to_text(example, formatting_fn):
 def format_regular(example):
     return f"Question: {example['question']}\nAnswer:"
 
+def format_cot(example):
+    return f"Q: {example['question']}\n\n    A: "
+
+def format_cot_llama(example):
+    return f"Given the following problem, reason and give a final answer to the problem.\nProblem: {example['question']}\nYour response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.\n"
+
+def format_cot_zeroshot(example):
+    return f"Q: {example['question']}\nA: Let's think step by step."
+
 doc_to_text = partial(_doc_to_text, formatting_fn=format_regular)
+doc_to_text_cot = partial(_doc_to_text, formatting_fn=format_cot)
+doc_to_text_cot_llama = partial(_doc_to_text, formatting_fn=format_cot_llama)
+doc_to_text_cot_zeroshot = partial(_doc_to_text, formatting_fn=format_cot_zeroshot)
